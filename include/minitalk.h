83278684c4bd26fc1e3378b62eb76353f8bbe9de
atan94 <amostan94@gmail.com>
2026-01-02 19:18:22 +0800

    Update minitalk.h header file: modify update timestamp, remove client_send_char declaration, and change fatal function parameter type

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 22:00:37 by amtan             #+#    #+#             */
-/*   Updated: 2026/01/01 19:35:21 by amtan            ###   ########.fr       */
+/*   Updated: 2026/01/02 19:11:39 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ typedef struct s_server_state
 }	t_server_state;
 
 void			client_send_message(pid_t server_pid, const char *msg);
-void			client_send_char(pid_t server_pid, unsigned char c);
 
 int				bit_from_signal(int sig);
 int				signal_from_bit(int bit);
@@ -32,6 +31,6 @@ unsigned char	set_bit(unsigned char byte, int bit_index, int bit_value);
 void			server_install_handlers(void);
 
 int				parse_pid(const char *s);
-void			fatal(const char *msg);
+void			fatal(char *msg);
 
 #endif
