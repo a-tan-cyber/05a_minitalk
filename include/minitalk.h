00d82070b94b50dea9beb09f57b2cbc5b9ae929f
atan94 <amostan94@gmail.com>
2026-01-02 19:35:22 +0800

    Update minitalk.h header file: modify update timestamp and adjust comment formatting

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 22:00:37 by amtan             #+#    #+#             */
-/*   Updated: 2026/01/02 19:11:39 by amtan            ###   ########.fr       */
+/*   Updated: 2026/01/02 19:30:14 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,18 @@ typedef struct s_server_state
 	pid_t			client_pid;
 }	t_server_state;
 
+/* src/client_send.c */
 void			client_send_message(pid_t server_pid, const char *msg);
 
+/* protocol_bits.c */
 int				bit_from_signal(int sig);
 int				signal_from_bit(int bit);
 unsigned char	set_bit(unsigned char byte, int bit_index, int bit_value);
 
+/* server_signal.c */
 void			server_install_handlers(void);
 
+/* utils.c */
 int				parse_pid(const char *s);
 void			fatal(char *msg);
 
