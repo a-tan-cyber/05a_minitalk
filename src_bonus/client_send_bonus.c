1d3fc2337e5e100c870b5f324c9aa5ef793aff9f
atan94 <amostan94@gmail.com>
2026-01-03 00:08:21 +0800

    feat: add bonus client and server signal handling implementation

---
+++ src_bonus/client_send_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_send_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 22:36:12 by amtan             #+#    #+#             */
+/*   Updated: 2026/01/02 23:22:32 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+#include <signal.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+#define MT_ACK_BIT 1
+#define MT_ACK_MSG 2
+
+static volatile sig_atomic_t	g_client_ack;
+
+static void	client_ack_handler(int sig)
+{
+	if (sig == SIGUSR1)
+		g_client_ack |= MT_ACK_BIT;
+	else if (sig == SIGUSR2)
+		g_client_ack |= MT_ACK_MSG;
+}
+
+static void	client_install_ack_handlers(void)
+{
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_handler = client_ack_handler;
+	if (sigemptyset(&sa.sa_mask) == -1
+		|| sigaddset(&sa.sa_mask, SIGUSR1) == -1
+		|| sigaddset(&sa.sa_mask, SIGUSR2) == -1
+		|| sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		fatal("minitalk: failed to install client ACK handlers");
+}
+
+static void	client_send_bit(pid_t server_pid, int bit)
+{
+	int	sig;
+
+	g_client_ack &= ~MT_ACK_BIT;
+	sig = signal_from_bit(bit);
+	if (sig == -1)
+		fatal("minitalk: invalid bit");
+	if (kill(server_pid, sig) == -1)
+		fatal("minitalk: failed to send signal");
+	while ((g_client_ack & MT_ACK_BIT) == 0)
+		pause();
+}
+
+static void	client_send_char(pid_t server_pid, unsigned char c)
+{
+	int	shift;
+	int	bit;
+
+	shift = 7;
+	while (shift >= 0)
+	{
+		bit = (c >> shift) & 1;
+		client_send_bit(server_pid, bit);
+		shift--;
+	}
+}
+
+void	client_send_message(pid_t server_pid, const char *msg)
+{
+	client_install_ack_handlers();
+	g_client_ack = 0;
+	while (*msg != '\0')
+	{
+		client_send_char(server_pid, (unsigned char)*msg);
+		msg++;
+	}
+	client_send_char(server_pid, '\0');
+	while ((g_client_ack & MT_ACK_MSG) == 0)
+		pause();
+}
---
+++ src_bonus/server_signal_bonus.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_signal_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 22:37:24 by amtan             #+#    #+#             */
+/*   Updated: 2026/01/02 22:56:02 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+#include <signal.h>
+#include <unistd.h>
+
+#include "libft.h"
+
+static t_server_state	g_svr;
+
+static void	server_reset_byte(void)
+{
+	g_svr.current_byte = 0;
+	g_svr.bit_index = 0;
+}
+
+static void	server_reset_state(void)
+{
+	server_reset_byte();
+	g_svr.client_pid = 0;
+}
+
+static void	server_signal_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	pid_t			sender;
+	int				bit;
+	unsigned char	c;
+
+	(void)ucontext;
+	sender = info->si_pid;
+	if (g_svr.client_pid != 0 && sender != g_svr.client_pid)
+		server_reset_state();
+	g_svr.client_pid = sender;
+	bit = bit_from_signal(sig);
+	g_svr.current_byte = set_bit(g_svr.current_byte, 7 - g_svr.bit_index, bit);
+	g_svr.bit_index++;
+	if (g_svr.bit_index == 8)
+	{
+		c = g_svr.current_byte;
+		if (c != '\0')
+			write(1, &c, 1);
+		server_reset_byte();
+		if (c == '\0')
+		{
+			g_svr.client_pid = 0;
+			kill(sender, SIGUSR2);
+		}
+	}
+	kill(sender, SIGUSR1);
+}
+
+void	server_install_handlers(void)
+{
+	struct sigaction	sa;
+
+	server_reset_state();
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = server_signal_handler;
+	if (sigemptyset(&sa.sa_mask) == -1
+		|| sigaddset(&sa.sa_mask, SIGUSR1) == -1
+		|| sigaddset(&sa.sa_mask, SIGUSR2) == -1
+		|| sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		fatal("minitalk: failed to install signal handlers");
+}
